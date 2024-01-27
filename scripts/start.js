eb55de940430849a31ad89529287088027c1a856
Abmael Rodrigo <abmaelrodrigo@MacBook-Air-de-Abmael.local>
2024-01-27 11:07:39 -0300

    seeting up 8080 port again

--- scripts/start.js
+++ scripts/start.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 let cmd = `serve --listen ${port} --no-request-logging`
 
 if (process.env.CI) cmd += ` --no-clipboard`
